25a21864c3f8941ca704eb9205adcf9bad5f5474
Tay <me@taylorrome.ro>
2014-07-29 14:12:35 -0600

    url updates

--- handbid.js
+++ handbid.js
@@ -87,7 +87,7 @@
         addScript = null,
         Class,
         host = '//handbid-js.local',
-        firebird = '//handbid.local:6789',
+        firebird = '//firebird.handbid.local:6789',
         cachebuster = 123456789, //for cdn and caching
         defaultOptions = { //default options the Handbid client will receive on instantiation
             dependencies: isBrowser ? ['//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js', host + '/lib/items.js?cachebuster=' + cachebuster, host + '/lib/Socket.io.js?cachebuster=' + cachebuster] : [],
