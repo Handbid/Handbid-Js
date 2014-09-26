d50a3045b9211188b7cf9def7a1b4f811e21fc8a
Tay <me@taylorrome.ro>
2014-09-26 17:27:18 -0600

    endpoint settings

--- handbid.js
+++ handbid.js
@@ -86,8 +86,8 @@
         isBrowser = typeof window !== 'undefined',
         addScript = null,
         Class,
-        host            = 'http://handbid-js.local', //where am i hosted and available to the planet?
-        firebird        = 'http://taysmacbookpro.local:6789',   //where is the firebird and where do i connect to it
+        host            = 'https://beta-js.hand.bid', //where am i hosted and available to the planet?
+        firebird        = 'https://beta-firebird.hand.bid:6789',   //where is the firebird and where do i connect to it
         connectEndpoint = 'https://beta-connect.hand.bid:8082',   //connect.handbid.com (where i send people to login/signup)
         cachebuster     = 123456789, //for cdn and caching (randomized by the "cache buster buster buster" on push)
         defaultOptions  = { //default options the Handbid client will receive on instantiation
